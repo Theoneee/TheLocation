33edb7b0d5fe39ca14b273e042404821f2907cff
theoneee <625805189@qq.com>
2019-07-11 14:22:59 +0800

    更换成高德定位

---
+++ app/src/main/java/the/one/test/Launcher.java
@@ -0,0 +1,35 @@
+package the.one.test;
+
+//  ┏┓　　　┏┓
+//┏┛┻━━━┛┻┓
+//┃　　　　　　　┃
+//┃　　　━　　　┃
+//┃　┳┛　┗┳　┃
+//┃　　　　　　　┃
+//┃　　　┻　　　┃
+//┃　　　　　　　┃
+//┗━┓　　　┏━┛
+//    ┃　　　┃                  神兽保佑
+//    ┃　　　┃                  永无BUG！
+//    ┃　　　┗━━━┓
+//    ┃　　　　　　　┣┓
+//    ┃　　　　　　　┏┛
+//    ┗┓┓┏━┳┓┏┛
+//      ┃┫┫　┃┫┫
+//      ┗┻┛　┗┻┛
+
+import the.one.base.base.activity.LauncherActivity;
+
+/**
+ * @author The one
+ * @date 2019/7/11 0011
+ * @describe TODO
+ * @email 625805189@qq.com
+ * @remark
+ */
+public class Launcher extends LauncherActivity {
+    @Override
+    protected Class getActivity() {
+        return MainActivity.class;
+    }
+}
